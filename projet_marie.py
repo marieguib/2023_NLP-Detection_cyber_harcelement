b751e47489c328d238ba3035567fd609a30a42c0
marieguib2 <marie2001.guibert@etudiant.univ-rennes2.fr>
2023-11-27 17:05:29 +0100

    projection

--- projet_marie.py
+++ projet_marie.py
@@ -3,3 +3,15 @@ import pandas as pd
 import os
 
 # Import de notre base de données
+data = pd.read_csv("Womens Clothing E-Commerce Reviews.csv", sep = ",")
+# print(data.columns)
+
+# Affichage des 5 premières lignes des colonnes où on a la partie textuelle
+# print(data[["Title", "Review Text"]].head())
+
+# 1) Nettoyage des données
+# On supprime les lignes où on a des valeurs manquantes pour Review Text
+# 
+
+# Choix de la problématique : 
+

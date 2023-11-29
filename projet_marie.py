37dd9e53093afca248be6a6fa9645c9740134b68
marieguib2 <marie2001.guibert@etudiant.univ-rennes2.fr>
2023-11-29 18:34:43 +0100

    supp ponctuation

--- projet_marie.py
+++ projet_marie.py
@@ -2,16 +2,70 @@
 import pandas as pd 
 import os
 
-# Import de notre base de données
 data = pd.read_csv("Womens Clothing E-Commerce Reviews.csv", sep = ",")
-# print(data.columns)
+data.head()
 
-# Affichage des 5 premières lignes des colonnes où on a la partie textuelle
-# print(data[["Title", "Review Text"]].head())
+nb_dpt = data.groupby("Department Name").count()
+nb_dpt
 
-# 1) Nettoyage des données
-# On supprime les lignes où on a des valeurs manquantes pour Review Text
-# 
+# On récupère les reviews
+avis = data["Review Text"]
 
-# Choix de la problématique : 
+# Découper les reviews en mots
+avis_clean = avis.str.split()
+avis_clean 
 
+# Tockenisation
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer('\w+')
+avis_clean = avis_clean.apply(lambda x: tokenizer.tokenize(str(x)))
+# print(avis_clean)
+
+# Traitement des formes élidés
+
+
+# Suppression de la ponctuation
+import string
+punct = string.punctuation
+print(punct)
+for pun in punct:
+    avis_clean = avis_clean.replace(pun, ' ')
+print(avis_clean)
+
+# Supprimer la ponctuation
+# import string
+# punct = string.punctuation
+# # print(punct)
+# for pun in punct:
+#     reviews_clean = reviews.replace(pun, ' ')
+# print(reviews_clean)
+
+
+# print(reviews.head())
+
+
+
+
+# # tockenisation
+# from nltk.tokenize import RegexpTokenizer
+# #Tockenisation avec Reg Exp
+# tokenizer = RegexpTokenizer('\w+')
+# tokenizer.tokenize('je suis ici.')
+
+# # suppression de la ponctuation
+# import string
+# punct = string.punctuation.replace('!', '')
+# for pun in punct:
+#     texte = texte.replace(pun, ' ')
+# texte
+
+# # suppression des caractères spéciaux
+# import re
+# texte = re.sub(r'\W', ' ', texte)
+# texte
+
+# # suppression des stopwords
+# import nltk
+# nltk.download('stopwords')
+# from nltk.corpus import stopwords
+# stopwords = stopwords.words('english')
